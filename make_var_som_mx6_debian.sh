a4676187ef561bef45b2d033e8ae33871ffa3d21
Nemanja Stefanovic <nemanja.stef92@gmail.com>
2020-02-29 18:44:33 +0100

    first working version of nano-edge custom hw

--- make_var_som_mx6_debian.sh
+++ make_var_som_mx6_debian.sh
@@ -40,7 +40,7 @@ readonly G_VARISCITE_PATH="${DEF_BUILDENV}/variscite"
 
 ## LINUX kernel: git, config, paths and etc
 readonly G_LINUX_KERNEL_SRC_DIR="${DEF_SRC_DIR}/kernel"
-readonly G_LINUX_KERNEL_GIT="https://github.com/varigit/linux-imx.git"
+readonly G_LINUX_KERNEL_GIT="https://github.com/netico-solutions/linux-imx.git"
 readonly G_LINUX_KERNEL_BRANCH="imx_4.9.88_2.0.0_ga-var01"
 readonly G_LINUX_KERNEL_REV="ec7d7d8ccb4960ee382c97f3b08db16086382d4b"
 readonly G_LINUX_KERNEL_DEF_CONFIG='imx_v7_var_defconfig'
