153044e8375412642f201c0cbbd030dd613ebefa
Nemanja Stefanovic <nemanja.stef92@gmail.com>
2020-03-10 05:31:27 +0100

    moving devel host

--- make_var_som_mx6_debian.sh
+++ make_var_som_mx6_debian.sh
@@ -43,8 +43,8 @@ readonly G_LINUX_KERNEL_SRC_DIR="${DEF_SRC_DIR}/kernel"
 readonly G_LINUX_KERNEL_GIT="https://github.com/netico-solutions/linux-imx.git"
 readonly G_LINUX_KERNEL_BRANCH="imx_4.9.88_2.0.0_ga-var01"
 readonly G_LINUX_KERNEL_REV="ec7d7d8ccb4960ee382c97f3b08db16086382d4b"
-readonly G_LINUX_KERNEL_DEF_CONFIG='imx_v7_var_defconfig'
-readonly G_LINUX_DTB='imx6dl-var-som-cap.dtb imx6dl-var-som-res.dtb imx6dl-var-som-solo-cap.dtb imx6dl-var-som-solo-res.dtb imx6dl-var-som-solo-vsc.dtb imx6dl-var-som-vsc.dtb imx6q-var-dart.dtb imx6q-var-som-cap.dtb imx6q-var-som-res.dtb imx6q-var-som-vsc.dtb imx6qp-var-som-cap.dtb imx6qp-var-som-res.dtb imx6qp-var-som-vsc.dtb'
+readonly G_LINUX_KERNEL_DEF_CONFIG='imx_v7_var_nanoedge_defconfig'
+readonly G_LINUX_DTB='imx6q-var-som-res.dtb'
 
 ## uboot
 readonly G_UBOOT_SRC_DIR="${DEF_SRC_DIR}/uboot"
