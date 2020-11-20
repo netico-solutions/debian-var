00b7390974d3c5a57fdc3976d0ebd55c4df52f26
Nenad Radulović <nenad.b.radulovic@gmail.com>
2020-11-20 16:15:41 +0100

    Make the script work for everyone

--- flash_uboot.sh
+++ flash_uboot.sh
@@ -1,8 +1,13 @@
 readonly G_UBOOT_NAME_FOR_EMMC='u-boot.img.mmc'
 readonly G_SPL_NAME_FOR_EMMC='SPL.mmc'
 
+# Get the absolute location of this file
+readonly local SCRIPT_PATH="dirname $(readlink -e $0)"
+
+# Set the output directory of the build script. This directory needs to match the value set over there.
+readonly local LPARAM_OUTPUT_DIR="${SCRIPT_PATH}/output"
 readonly local LPARAM_BLOCK_DEVICE=$1
-readonly local LPARAM_OUTPUT_DIR="/home/lazar/var_som_mx6_debian/output"
+
 # pr_info "Flashing U-Boot"
 echo "Flashing U-Boot"
 dd if=${LPARAM_OUTPUT_DIR}/${G_SPL_NAME_FOR_EMMC} of=${LPARAM_BLOCK_DEVICE} bs=1K seek=1; sync
